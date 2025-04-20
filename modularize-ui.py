e85b9112666c01200a3c806495970ad911b547e0
Nathan aka SoCalGuitarist <114831821+SanDiegoDude@users.noreply.github.com>
2025-04-19 23:55:12 -0700

    Update modularize-ui.py

--- modularize-ui.py
+++ modularize-ui.py
@@ -320,25 +320,25 @@ def worker(
             output_filename = os.path.join(outputs_folder, f'{job_id}_{total_generated_latent_frames}.mp4')
 
             if mode == "text2video" and is_last_section and latent_window_size == 2:
-            debug("txt2img branch: pulling last frame, skipping video trim")
-            # Make sure we have decoded at least one frame!
-            if history_pixels is None:
-                debug("txt2img: running vae_decode for single patch")
-                history_pixels = vae_decode(real_history_latents, vae).cpu()
-            # Defensive check:
-            if history_pixels.shape[2] > 0:
-                last_img_tensor = history_pixels[0, :, -1]  # shape [3, H, W]
-                last_img = np.clip((np.transpose(last_img_tensor.cpu().numpy(), (1, 2, 0)) + 1) * 127.5, 0, 255).astype(np.uint8)
-                img_filename = os.path.join(outputs_folder, f'{job_id}_final_image.png')
-                Image.fromarray(last_img).save(img_filename)
-                html_link = f'<a href="file/{img_filename}" target="_blank"><img src="file/{img_filename}" style="max-width:100%;border:3px solid orange;border-radius:8px;" title="Click for full size"></a>'
-                stream.output_queue.push(('file_img', (img_filename, html_link)))
-                stream.output_queue.push(('end', None))
-                return
-            else:
-                debug("txt2img: ERROR: No frames were decoded! This should not happen.")
-                stream.output_queue.push(('end', None))
-                return
+                debug("txt2img branch: pulling last frame, skipping video trim")
+                # Make sure we have decoded at least one frame!
+                if history_pixels is None:
+                    debug("txt2img: running vae_decode for single patch")
+                    history_pixels = vae_decode(real_history_latents, vae).cpu()
+                # Defensive check:
+                if history_pixels.shape[2] > 0:
+                    last_img_tensor = history_pixels[0, :, -1]  # shape [3, H, W]
+                    last_img = np.clip((np.transpose(last_img_tensor.cpu().numpy(), (1, 2, 0)) + 1) * 127.5, 0, 255).astype(np.uint8)
+                    img_filename = os.path.join(outputs_folder, f'{job_id}_final_image.png')
+                    Image.fromarray(last_img).save(img_filename)
+                    html_link = f'<a href="file/{img_filename}" target="_blank"><img src="file/{img_filename}" style="max-width:100%;border:3px solid orange;border-radius:8px;" title="Click for full size"></a>'
+                    stream.output_queue.push(('file_img', (img_filename, html_link)))
+                    stream.output_queue.push(('end', None))
+                    return
+                else:
+                    debug("txt2img: ERROR: No frames were decoded! This should not happen.")
+                    stream.output_queue.push(('end', None))
+                    return
 
             
             # TEXT2VIDEO --------
