34e55ad2fd09f8add1924bbbcd97765bd21109b7
Nathan aka SoCalGuitarist <114831821+SanDiegoDude@users.noreply.github.com>
2025-04-25 09:06:58 -0700

    Update common.py

--- utils/common.py
+++ utils/common.py
@@ -1,1 +1,20 @@
-# General helpers like debug logging
+# utils/common.py
+# General utility functions
+
+# Debug control
+DEBUG = True
+
+def setup_debug(enable=True):
+    """Set up debugging mode"""
+    global DEBUG
+    DEBUG = enable
+
+def debug(*args, **kwargs):
+    """Print debug messages when DEBUG is enabled"""
+    if DEBUG:
+        print("[DEBUG]", *args, **kwargs)
+
+def generate_timestamp():
+    """Generate a timestamp for file naming"""
+    import time
+    return time.strftime("%Y%m%d_%H%M%S")
