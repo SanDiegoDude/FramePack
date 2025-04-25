9b3b1290148d57c8eaed6691af03c00996826016
Nathan aka SoCalGuitarist <114831821+SanDiegoDude@users.noreply.github.com>
2025-04-25 08:27:04 -0700

    Create interface.py

---
+++ ui/interface.py
@@ -0,0 +1,10 @@
+def create_interface(model_manager, video_generator):
+    """Create and configure the Gradio interface"""
+    block = gr.Blocks(css=css).queue()
+    
+    with block:
+        # Define UI components
+        # Connect callbacks
+        pass
+        
+    return block
