23c5f0106a6c5dc0477131fc00d8c8ff91ec3c0f
Nathan aka SoCalGuitarist <114831821+SanDiegoDude@users.noreply.github.com>
2025-05-03 09:29:58 -0700

    Update interface.py

--- ui/interface.py
+++ ui/interface.py
@@ -332,12 +332,12 @@ function addLightboxListeners() {
                        clear_cache_button = gr.Button(value="Clear CUDA Cache", variant="secondary")
                   mem_status = gr.Markdown("")
              with gr.Row(equal_height=True) as top_row:
-                start_button = gr.Button(value="Start Generation", elem_classes="start-button start-button-wide")
-                batch_count = gr.Number(
+                  start_button = gr.Button(value="Start Generation", elem_classes="start-button start-button-wide")
+                  batch_count = gr.Number(
                     value=1, minimum=1, step=1, precision=0,
                     label="Batch", show_label=True, container=True,
                     elem_classes="batch-input-narrow"
-                )
+                  )
                   
                   with gr.Row():
                        endless_run_button = gr.Button(value="Endless Run", elem_classes="endless-run-button")
